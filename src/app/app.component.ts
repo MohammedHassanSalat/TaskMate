7b9a287d47b5f9d376a65419358c2f16057adaac
MohammedHassanSalat <mohh20033@gmail.com>
2024-12-31 21:39:57 +0200

    setup environment

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,53 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-
+import { Component, OnInit } from '@angular/core';
+import { PrimeNG } from 'primeng/config';
+import Aura from '@primeng/themes/aura';
+import { FilterMatchMode } from 'primeng/api';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'task-mate';
+
+  constructor(private primeng: PrimeNG) {
+    this.primeng.theme.set({
+      preset: Aura,
+      options: {
+        cssLayer: {
+          name: 'primeng',
+          order: 'tailwind-base, primeng, tailwind-utilities',
+        },
+      },
+    });
+  }
+
+  ngOnInit(): void {
+    this.primeng.filterMatchModeOptions = {
+      text: [
+        FilterMatchMode.STARTS_WITH,
+        FilterMatchMode.CONTAINS,
+        FilterMatchMode.NOT_CONTAINS,
+        FilterMatchMode.ENDS_WITH,
+        FilterMatchMode.EQUALS,
+        FilterMatchMode.NOT_EQUALS,
+      ],
+      numeric: [
+        FilterMatchMode.EQUALS,
+        FilterMatchMode.NOT_EQUALS,
+        FilterMatchMode.LESS_THAN,
+        FilterMatchMode.LESS_THAN_OR_EQUAL_TO,
+        FilterMatchMode.GREATER_THAN,
+        FilterMatchMode.GREATER_THAN_OR_EQUAL_TO,
+      ],
+      date: [
+        FilterMatchMode.DATE_IS,
+        FilterMatchMode.DATE_IS_NOT,
+        FilterMatchMode.DATE_BEFORE,
+        FilterMatchMode.DATE_AFTER,
+      ],
+    };
+  }
 }
